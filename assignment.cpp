23ded579c4dc2c7f935ea98085d5af9f165a61cd
amalendu02 <amalendumaiti14@gmail.com>
2024-05-24 22:18:30 +0530

    m

--- assignment.cpp
+++ assignment.cpp
@@ -15,7 +15,7 @@ int main () {
         else if (student > assignment){
             cout<<"Assignment is done , but not submitted" << endl;
         }
-        else {jn
+        else {
             cout<<"Assignment is NOT done"<<endl;
         }
     return 0;
