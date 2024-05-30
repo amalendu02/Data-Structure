d040c6520015f23b52b6657ddf26b2108dc2b0c7
amalendu02 <amalendumaiti14@gmail.com>
2024-05-30 22:54:15 +0530

    binary-search-practice

---
+++ searching/Binary-Search-1s.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int start = 0;
+    int end = n-1;
+    int mid = (start + end) / 2;
+
+    while(start <= end) {
+        if(arr[mid] == target) {
+            return mid;
+        }
+        else if( target > arr[mid]) {
+            start = mid+1;
+        }
+        else if( target < arr[mid]) {
+            end = mid-1;
+        }
+
+        // new to update the new mid 
+        mid = (start + end) / 2;
+    }
+
+    // if we search all array and found nothing , we are here in this block then return -1
+
+    return -1;
+}
+int main () {
+    int arr[] = {10,20,30,40,50,60,70,80,90};
+    int target = 60;
+    int n = 9;
+
+    int findIndex = binarySearch(arr,n,target);
+
+    if (findIndex == -1) {
+        cout << " element not found " << endl;
+    }
+
+    else{
+        cout << " element found Index No: " << findIndex << endl;
+    }
+}
