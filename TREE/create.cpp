916fc024afa7233c2d3c57d480e2898805a23b25
amalendu02 <amalendumaiti14@gmail.com>
2024-10-16 23:08:25 +0530

    tree

---
+++ TREE/create.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+
+// its return root node of created tree
+    Node* createTree() {
+        cout << "Enter the valeue: " << endl;
+        int data;
+        cin >> data;
+
+        // when user giving -1 in input it means NULL
+        if(data == -1) {
+            return NULL;
+        }
+
+        // NEWNODE means ROOT NODE
+        // create node
+        Node* newNode = new Node(data);
+
+        // create left subtree
+        newNode->left = createTree();
+
+        //create right subtree
+        newNode->right = createTree();
+        return newNode;
+    }
+
+int main () {
+
+    Node* root = createTree();
+    cout <<" Root Node is: "<<root->data << endl;
+    return 0;
+}
